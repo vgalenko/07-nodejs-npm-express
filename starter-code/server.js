c9af5127930bb16357a20789224bd0fec4a1a9a8
jensjoh01 <johnnyj2439@gmail.com>
2017-08-22 14:13:34 -0700

    finished lab

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
@@ -11,10 +11,13 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
-
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new', function(req, res) {
+  console.log('going to new html')
+  res.sendFile('new.html', {root: './public'})
+})
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -25,5 +28,6 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('listening on port', PORT);
 });
